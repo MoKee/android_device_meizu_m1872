c83c9227795049b92564044b7dbcaf6e961d621d
XiNGRZ <chenxingyu92@gmail.com>
2020-03-22 01:49:18 +0800

    m1872: Move keylayout here
    Change-Id: I7482f48e136054c34e69f6a74e5bdf5ac30cc4c7

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/init/vendor,$(TARGET_COPY_OUT_VENDOR))
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/keylayout,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.m1872
